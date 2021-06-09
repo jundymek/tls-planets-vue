6c22bb8103538c3f380e3150142c10b235e20097
jundymek <jundymek@gmail.com>
2021-06-09 10:19:42 +0200

    feat: Planet component

--- src/components/planets/Planets.vue
+++ src/components/planets/Planets.vue
@@ -1,20 +1,27 @@
 <template>
-  <h1 class="title">Recruitment task - planets</h1>
+  <div>
+    <h1 class="title">Recruitment task - planets</h1>
+    <div v-for="planet in planets" :key="planet.name">
+      <Planet :planet="planet"/>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Planet from './planet/Planet.vue'
 export default {
   name: 'Planets',
+  components: { Planet },
   data () {
     return {
       planets: null
     }
   },
-  mounted () {
+  async mounted () {
     axios.get('https://swapi.dev/api/planets/').then(response => {
       console.log(response.data.results)
-      this.planets = response
+      this.planets = response.data.results
     })
   }
 }
---
+++ src/components/planets/planet/Planet.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <p>{{ name }}</p>
+    <p>{{ rotationPeriod }}</p>
+    <p>{{ climate }}</p>
+    <p>{{ gravity }}</p>
+    <p>{{ created }}</p>
+    <p>{{ url }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['planet'],
+  data: function () {
+    return {
+      name: this.planet.name,
+      rotationPeriod: this.planet.rotation_period,
+      climate: this.planet.climate,
+      gravity: this.planet.gravity,
+      created: new Date(Date.parse(this.planet.created)).toLocaleString(),
+      url: this.planet.url
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
