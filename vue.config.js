8fabc2a319dcdf1128c72dd5964e0c341e5671d3
jundymek <jundymek@gmail.com>
2021-06-15 10:47:11 +0200

    chore: gh-pages config

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ const path = require('path')
 const resolve = dir => path.join(__dirname, dir)
 
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/tls-planets-vue/' : '/',
   configureWebpack: {
     resolve: {
       extensions: ['.js', '.vue', '.json'],
