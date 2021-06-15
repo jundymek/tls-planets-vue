14f26647a84a032d007858d10cdc907d281915be
jundymek <jundymek@gmail.com>
2021-06-15 10:04:14 +0200

    style: Planet hover effect

--- src/components/planets/planet/Planet.vue
+++ src/components/planets/planet/Planet.vue
@@ -1,36 +1,38 @@
 <template>
-  <el-card class="box-card">
-    <div class="planet-header">
-      <h2 class="planet-header__title" :style="{ color: styles.color }">
-        {{ name }}
-      </h2>
-      <p class="planet-header__subtitle">{{ climate }} planet</p>
-    </div>
-    <div class="planet-image-wrapper" :style="{ 'border-color': styles.color }">
-      <div
-        class="planet-image"
-        :style="{
-          'background-image':
-            'url(' + require(`@/assets/images/${styles.image}`) + ')',
-        }"
-      ></div>
-    </div>
-    <ul class="planet-data">
-      <li
-        v-for="(value, key) in {
-          'Rotation period': rotationPeriod,
-          Climate: climate,
-          Gravity: gravity,
-          Created: created,
-        }"
-        :key="key"
-        class="text item"
-      >
-        {{ key }} - <span :style="{ color: styles.color }">{{ value }}</span>
-      </li>
-      <li class="text item">Url: <a :href="url" class="planet-data__url" :style="{ color: styles.color }">{{url}}</a></li>
-    </ul>
-  </el-card>
+  <a :href="url" class="box-card-url" :title="url">
+    <el-card>
+      <div class="planet-header">
+        <h2 class="planet-header__title" :style="{ color: styles.color }">
+          {{ name }}
+        </h2>
+        <p class="planet-header__subtitle">{{ climate }} planet</p>
+      </div>
+      <div class="planet-image-wrapper" :style="{ 'border-color': styles.color }">
+        <div
+          class="planet-image"
+          :style="{
+            'background-image':
+              'url(' + require(`@/assets/images/${styles.image}`) + ')',
+          }"
+        ></div>
+      </div>
+      <ul class="planet-data">
+        <li
+          v-for="(value, key) in {
+            'Rotation period': rotationPeriod,
+            Climate: climate,
+            Gravity: gravity,
+            Created: created,
+          }"
+          :key="key"
+          class="text item"
+        >
+          {{ key }} - <span :style="{ color: styles.color }">{{ value }}</span>
+        </li>
+        <li class="text item">Url: <a :href="url" class="planet-data__url" :style="{ color: styles.color }">{{url}}</a></li>
+      </ul>
+    </el-card>
+  </a>
 </template>
 
 <script>
@@ -63,6 +65,9 @@ export default {
 </script>
 
 <style lang="scss">
+.box-card-url {
+  text-decoration: none;
+}
 .el-card {
   border-radius: 0;
   border-color: $border-color;
@@ -70,6 +75,13 @@ export default {
   color: $text-color;
   height: 100%;
   min-height: 450px;
+  transition: all .2s ease;
+  transition-property: transform, filter;
+  cursor: pointer;
+  &:hover {
+    transform: scale(1.02);
+    filter: brightness(140%);
+  }
 
   &__body {
     display: flex;
