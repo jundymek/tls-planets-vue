3c209b143233e01c5030fb621b332e16f127bb8b
jundymek <jundymek@gmail.com>
2021-06-18 11:38:59 +0200

    feat: add 429 status info with starwars font

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,27 @@
 </template>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap');
+@import url("https://fonts.googleapis.com/css2?family=Source+Code+Pro&display=swap");
+@font-face {
+  font-family: "StarWars";
+  src: local("StarWars"),
+    url(~@/assets/fonts/StarWars/starwars.woff) format("woff");
+}
 
 * {
-  font-family: 'Source Code Pro', monospace;
+  font-family: "Source Code Pro", monospace;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
 
 body {
   background: $background-color;
-  background-image: url('../src/assets/images/sky_background1.jpeg');
+  background-image: url("../src/assets/images/sky_background1.jpeg");
   background-blend-mode: color-burn;
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   overflow-x: hidden;
-  height:100%;
+  height: 100%;
 }
 </style>
--- src/components/Planets/component.js
+++ src/components/Planets/component.js
@@ -50,8 +50,11 @@ export default {
         this.isLoading = false
         if (error.response.status === 404) {
           this.$router.push('404')
+        } else if (error.response.status === 429) {
+          this.error = "Dear Recruiter. Unfortunately, due to the intensive work on the task, I made so many requests to the API that I obtained the status of 429, which means that the planets will not appear in the place intended for this. I can assure you that I made every effort to make everything work as it should, but unfortunately I couldn't predict everything. API limitations and such things just happen. I hope that in a few hours the situation will return to normal and you will be able to see the effects of my work ..."
+        } else {
+          this.error = error.message
         }
-        this.error = error.message
         console.log(error.message)
       }
     },
@@ -77,7 +80,7 @@ export default {
     this.fetchAllPlanets()
   },
   watch: {
-    $route (to, from) {
+    $route () {
       if (this.filteredPlanets.length !== this.planets.length) {
         this.currentPagePlanets = this.filteredPlanets.slice((this.$route.params.id - 1) * 12, ((this.$route.params.id - 1) * 10) + 12)
       } else {
