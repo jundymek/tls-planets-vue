4a25569c1cba629834f4867fbf2e5825532d9a38
jundymek <jundymek@gmail.com>
2021-06-20 21:54:58 +0200

    fix: Header link clickable only on logo

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="header">
-    <a :href="`${publicPath}`" class="header__link" title="Home">
-      <h1 class="header__title">
+    <h1 class="header__title">
+      <a :href="`${publicPath}`" class="header__link" title="Home">
         <div class="header__image-wrapper">
           <img
             src="@/assets/images/death_star.png"
@@ -13,8 +13,8 @@
           <span class="header__title-text--first">SWAPI</span>
           <span class="header__title-text--second"> planets</span>
         </div>
-      </h1>
-    </a>
+      </a>
+    </h1>
   </header>
 </template>
 <style src="./style.scss" lang="scss" scoped></style>
